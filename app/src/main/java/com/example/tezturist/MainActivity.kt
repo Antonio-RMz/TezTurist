33cbd4ff8f81c5ca9508a73b035cda814a086274
Marco20TE0541 <rodriguezmendezmarcoantonioh@gmail.com>
2023-11-10 19:03:51 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tezturist/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.tezturist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupNavegacion()
+    }
+    private fun setupNavegacion() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_hostfragmet) as NavHostFragment?
+        setupWithNavController(
+            bottomNavigationView,
+            navHostFragment!!.navController
+        )
+    }
+}

321a750defb08886a8881f7922ab7d68aebc4de2
Marco20TE0541 <rodriguezmendezmarcoantonioh@gmail.com>
2023-11-20 00:52:14 -0600

    Version 7 app con inicio de sesio, consumo de firebase, y consumo de api Google maps

--- app/src/main/java/com/example/tezturist/FragmentTres.java
+++ app/src/main/java/com/example/tezturist/FragmentTres.java
@@ -1,60 +1,62 @@
 package com.example.tezturist;
 //fragmento donde se ve la informacion de firebase
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.FirebaseDatabase;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FragmentTres extends Fragment {
+    private FirebaseAuth auth;
+    private Button button;
+    private TextView textViewEmail;
 
-    RecyclerView recyclerView;
-    MainAdapterTres mainAdapter;
+    private FirebaseUser user;
 
-    public FragmentTres() {
-        // Required empty public constructor
-    }
-
-//segun yo este fragment tres no sirve pa nada
-    public static FragmentTres newInstance(String param1, String param2) {
-        FragmentTres fragment = new FragmentTres();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tres, container, false);
 
-        recyclerView = view.findViewById(R.id.rv);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        auth = FirebaseAuth.getInstance();
+        button = view.findViewById(R.id.cerrarSesion);
+        textViewEmail = view.findViewById(R.id.textEmail);
 
-        FirebaseRecyclerOptions<MainModelTres> options =
-                new FirebaseRecyclerOptions.Builder<MainModelTres>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("sitios"), MainModelTres.class)
-                        .build();
-        mainAdapter = new MainAdapterTres(options);
-        recyclerView.setAdapter(mainAdapter);
+        // Iniciará al usuario actual
+        user = auth.getCurrentUser();
+        if (user == null) {
+            // Condición que manda al login si no hay usuario, se finaliza la actividad y se manda a ActivityLogin
+            Intent intent = new Intent(requireContext(), ActivityLogin.class);
+            startActivity(intent);
+            requireActivity().finish();
+        } else {
+            // Configura el correo electrónico del usuario
+            textViewEmail.setText(user.getEmail());
+        }
+
+        // Detector al hacer clic en el botón cerrar sesión
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Aquí se cierra la sesión
+                FirebaseAuth.getInstance().signOut();
+                // Cerrar la actividad actual y abrir la de inicio de sesión
+                // Condición que manda al login si no hay usuario, se finaliza la actividad y se manda a ActivityLogin
+                Intent intent = new Intent(requireContext(), ActivityLogin.class);
+                startActivity(intent);
+                requireActivity().finish();
+            }
+        });
 
         return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mainAdapter.startListening();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mainAdapter.stopListening();
     }
 }
